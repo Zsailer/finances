8a0907cc635d4a56b69991ca11586d400f8a8c40
Zsailer <zachsailer@gmail.com>
2019-01-08 22:04:55 -0800

    update mortgage loan object

--- finances/mortgage.py
+++ finances/mortgage.py
@@ -1,10 +1,14 @@
+from .objects import Dollars
 from .realestate import (
     total_mortgage_breakdown,
     mortgage_payment
 )
 
 class LiveMortgageLoan:
-    """Explore a live mortgage."""
+    """Explore a live mortgage.
+    
+    This object assumes an amortized loan. 
+    """
     def __init__(self, original_price,
         frac_down=0.0,
         years_of_loan=30,
@@ -12,11 +16,11 @@ class LiveMortgageLoan:
         average_appreciate_rate=0.05,
         current_year=0
         ):
-        self.original_price = original_price
+        self.original_price = Dollars(original_price)
         self.frac_down = frac_down
         self.annual_interest_rate = annual_interest_rate
         self.years_of_loan = years_of_loan
-        self.average_appreciate_rate = average_appreciate_rate,
+        self.average_appreciate_rate = average_appreciate_rate
         self.current_year = current_year
 
     def __repr__(self):
@@ -27,14 +31,15 @@ class LiveMortgageLoan:
             years=self.years_of_loan,
             annual_rate=self.annual_interest_rate
         )
-        print(f"Price of the house:    {self.original_price}")
-        print(f"Money down:            {down}")
-        print(f"Estimated interest:    {interest}")
-        print(f"Equity earned:         {equity}")
-        print(f"Monthly mortgage:      {self.monthly_payment}")
-        print("")
-        print(f"Predicted value in 5 years:     {self.predicted_value(5)}")
-        print(f"Predicted profit in 5 years:    {self.predicted_profit(5)}")
+        return (
+            f"Price of the house:    {self.original_price}\n"
+            f"Money down:            {down}\n"
+            f"Estimated interest:    {interest}\n"
+            f"Monthly mortgage:      {self.monthly_payment}\n"
+            "\n"
+            f"Predicted value in 5 years:     {self.predicted_appreciation(5)}\n"
+            f"Predicted profit in 5 years:    {self.predicted_profit(5)}"
+        )
 
     @property
     def monthly_payment(self):
@@ -43,9 +48,12 @@ class LiveMortgageLoan:
             frac_down=self.frac_down,
             years=self.years_of_loan,
             annual_rate=self.annual_interest_rate
-        )
+        )[0]
 
-    def predicted_value(self, current_year=None, average_appreciate_rate=None):
+    def total_paid(self, current_year=None):
+        return self.monthly_payment * 12 * current_year
+
+    def predicted_appreciation(self, current_year=None, average_appreciate_rate=None):
         """Predict the value of the house after N (current) years."""
         if current_year is None:
             current_year = self.current_year
@@ -56,24 +64,31 @@ class LiveMortgageLoan:
             price *= 1 + average_appreciate_rate
         return price
 
+    def predicted_leftover_principal(self, current_year=None):
+        """"""
+        if current_year is None:
+            current_year = self.current_year
+        monthly_rate = self.annual_interest_rate / 12
+        monthly_payment = self.monthly_payment
+        principal = self.original_price
+        for month in range(current_year * 12):
+            interest_payment = principal * monthly_rate
+            principal_paid = monthly_payment - interest_payment
+            principal -= principal_paid
+        return principal
+
     def predicted_profit(self, current_year=None, average_appreciate_rate=None):
         """Calculate the predicted profit.
         """
-        # Calculate a predicted price.
-        predicted_price = self.predicted_value(
-            current_year=current_year,
-            average_appreciate_rate=average_appreciate_rate
-        )
-        # Appreciation
-        appreciation = predicted_price - self.original_price    
-        total_payments = current_year * 12 * self.monthly_payment
-
-        # HARDCODED... I need to look this up!
-        # fraction of monthly_payment that goes to principle (not interest)
-        frac_payment_to_principal = 0.9
-        equity = current_year * 12 * (self.monthly_payment * frac_payment_to_principal)
-                
-
+        appreciated_principal = self.predicted_appreciation(
+            current_year=current_year, 
+            average_appreciate_rate=average_appreciate_rate)
+        predicted_leftover = self.predicted_leftover_principal(
+            current_year=current_year)
+        total_paid = self.total_paid(current_year=current_year)
+        equity = appreciated_principal - predicted_leftover
+        profit = equity - total_paid
+        return profit 
 
 
 class RentalMortgage(LiveMortgageLoan):
